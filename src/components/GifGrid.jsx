3b713ebff1e451216d273561b38e98653504f46e
Andres Castillo <ansa2000@hotmail.es>
2022-10-14 09:36:04 -0500

    Mostrando mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -5,12 +5,14 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 export const GifGrid = ({ category }) => {
 
     const { images, isLoading } = useFetchGifs( category );
-
-    console.log({ isLoading });
     
     return (
         <>
             <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+    
+            }
 
             <div className="card-grid">
                 {
